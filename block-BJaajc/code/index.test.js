77b91230e254d902d06b1d346f4dbc76755d26cb
KASHISH <kashu80525@gmail.com>
2023-09-27 11:28:40 +0530

    adding

--- block-BJaajc/code/index.test.js
+++ block-BJaajc/code/index.test.js
@@ -3,3 +3,7 @@ const { fullName } = require("./index");
 test("fullName Arya + Stark is equal AryaStark", () => {
   expect(fullName(Arya, Stark)).toBe(AryaStark);
 });
+
+test("fullName John + Stark is equal JohnStark", () => {
+  expect(fullName(John, Stark)).toBe(JohnStark);
+});
